2a24c1b517707a4905d91b2b0939bcd3b16bb53b
Zyl <mk816017@gmail.com>
2019-07-22 04:34:17 +0800

    QQQ

--- src/pages/p/[id].js
+++ src/pages/p/[id].js
@@ -1,14 +1,15 @@
 import Layout from '@components/Layout'
+import fetch from 'isomorphic-unfetch'
 
-const Post = props => (
+const Post = ({ show }) => (
   <Layout>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
-    <img src={props.show.image.medium} alt={props.show.name} />
+    <h1>{show.name}</h1>
+    <p>{show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={show.image.medium} alt={show.name} />
   </Layout>
 )
 
-Post.getInitialProps = async function(context) {
+Post.getInitialProps = async context => {
   const { id } = context.query
   const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
   const show = await res.json()
